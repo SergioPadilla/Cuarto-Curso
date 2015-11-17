cc452f88becfb93d5271e529a46fa576fc524f72
SergioPadilla <splsergiopadilla@gmail.com>
2015-11-17 12:10:33 +0100

    Borrando directorio anterior

--- Informatica-Grafica/Práctica4/srcs-prac/figura.cpp
+++ Informatica-Grafica/Práctica4/srcs-prac/figura.cpp
@@ -8,11 +8,8 @@ void Figura::fijarh(float h1){
 Figura::Figura(float h1){
   fijarh(h1);
   agregar(new Base());
-
-  //agregar(MAT_Traslacion(0.0,5.5,0.0));
-  //glutSolidSphere(1.0,50,50);
+  //agregar(new Brazo1(5));
   agregar(new Brazo2(5));
-  agregar(new Brazo1(5));
   agregar(MAT_Traslacion(0.0,15.5,0.0));
   agregar(new Tetraedro());
 }
