ef945f159e001847070fca649f7228122ebf0cd5
SergioPadilla <splsergiopadilla@gmail.com>
2015-12-11 03:43:40 +0100

    Archivos para clase MaterialEstandar

---
+++ Informatica-Grafica/Practica4/srcs-prac/MaterialEstandar.cpp
@@ -0,0 +1,7 @@
+#include "MaterialEstandar.hpp"
+
+void MaterialEstandar::activar(){
+  // activa material en el cauce fijo
+  //   si tiene textura: activa uso de la textura 'text'
+  //   si no tiene textura: desactiva uso de texturas
+}
---
+++ Informatica-Grafica/Practica4/srcs-prac/MaterialEstandar.hpp
@@ -0,0 +1,22 @@
+#ifndef MATERIAL_ESTANDAR_HPP
+#define MATERIAL_ESTANDAR_HPP
+
+#include <vector>
+#include <cctype>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include "tuplasg.hpp"
+#include "aux.hpp"
+
+class MaterialEstandar : public Material{
+public:
+  Textura * text;        // puntero a la textura (NULL si no hay)
+  Tupla4f color[4];    // colores: 0=emisión, 1=ambiental, 2=difuso, 3=especular
+  float exponente;   // exponente para la componente pseudo-especular
+
+  virtual void activar();
+};
+
+#endif
