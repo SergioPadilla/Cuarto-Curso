af9177ada7ee5bcd86e13f0682f7bf4934efe4a4
SergioPadilla <splsergiopadilla@gmail.com>
2015-12-13 16:37:26 +0100

    Comienzo de implementacion Textura.cpp, aún sin terminar

--- Informatica-Grafica/Practica4/srcs-prac/Textura.cpp
+++ Informatica-Grafica/Practica4/srcs-prac/Textura.cpp
@@ -1,9 +1,27 @@
 #include "Textura.hpp"
 
 Textura::Textura(const std::string & archivoJPG){
+  glGenTextures(1, &id_text);
+  
+
   // crea un textura a partir de un archivo
 }
 
 void Textura::activar(){
+  glEnable(GL_TEXTURE_2D);
+  glBindTexture(GL_TEXTURE_2D, id_text); //activa textura id_text
+  glLightModeli(GL_LIGHT_MODEL_COLOR_CONTROL, GL_SEPARATE_SPECULAR_COLOR);
+
+  if(mgct != 0){
+    glEnable(GL_TEXTURE_GEN_S); //Para generar las cc.tt. en cada pixel
+    glEnable(GL_TEXTURE_GEN_T); //Para generar las cc.tt. en cada pixel
+    if(mgct == 1){
+      glTexGenfv(GL_S, GL_OBJECT_PLANE, cs);
+      glTexGenfv(GL_T, GL_OBJECT_PLANE, ct);
+    }else{
+      glTexGenfv(GL_S, GL_EYE_PLANE, cs);
+      glTexGenfv(GL_T, GL_EYE_PLANE, ct);
+    }
+  }
   // activa textura en el cauce fijo de OpenGL
 }
--- Informatica-Grafica/Practica4/srcs-prac/Textura.hpp
+++ Informatica-Grafica/Practica4/srcs-prac/Textura.hpp
@@ -13,8 +13,8 @@
 class Textura{
 public:
   GLuint id_text;    // identificador OpenGL de la textura
-  jpg::Imagen * img;        // puntero a imagen con los texels
-  unsigned mgct;       // modo de gen. cc.tt. (0 = desactivado, 1 = objeto, 2 = camara)
+  jpg::Imagen * img; // puntero a imagen con los texels
+  unsigned mgct;     // modo de gen. cc.tt. (0 = desactivado, 1 = objeto, 2 = camara)
   float cs[4],ct[4]; // coeficientes para generación de coords. de textura
 
   void activar();          // activa textura en el cauce fijo de OpenGL
