12a81c453c1d4ad34519576a96115bbee4f76355
SergioPadilla <splsergiopadilla@gmail.com>
2015-12-13 14:19:20 +0100

    Implementación activar MaterialEstandar

--- Informatica-Grafica/Practica4/srcs-prac/MaterialEstandar.cpp
+++ Informatica-Grafica/Practica4/srcs-prac/MaterialEstandar.cpp
@@ -1,7 +1,9 @@
 #include "MaterialEstandar.hpp"
 
 void MaterialEstandar::activar(){
-  // activa material en el cauce fijo
-  //   si tiene textura: activa uso de la textura 'text'
-  //   si no tiene textura: desactiva uso de texturas
+  glMaterialf(GL_FRONT_AND_BACK, color[0]); // Me
+  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, color[1]); //MA
+  glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color[2]); //MD
+  glMaterialfv( GL_FRONT_AND_BACK, GL_SPECULAR, color[3]); //MS
+  glMaterialf( GL_FRONT_AND_BACK, GL_SHININESS, exponente) ;
 }
